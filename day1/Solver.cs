dc905b1f0ffc66be28389f9e9fb41ab8f49fb9a5
Wolfgang Weintritt <wolfgang.weintritt@gmail.com>
2024-12-01 11:38:20 +0100

    Add day1 solution

---
+++ day1/Solver.cs
@@ -0,0 +1,18 @@
+﻿string filePath = "input.txt";
+string[] lines = File.ReadAllLines(filePath);
+
+Func<string, int, int> selectIdxFromLine =
+    (string line, int x) => int.Parse(line.Split(' ', StringSplitOptions.RemoveEmptyEntries)[x]);
+int[] leftColumn = lines.Select(line => selectIdxFromLine(line, 0)).ToArray();
+int[] rightColumn = lines.Select(line => selectIdxFromLine(line, 1)).ToArray();
+
+// Part1
+int[] leftColumnSorted = leftColumn.OrderBy(x => x).ToArray();
+int[] rightColumnSorted = rightColumn.OrderBy(x => x).ToArray();
+
+int diffSum = leftColumnSorted.Zip(rightColumnSorted, (x, y) => Math.Abs(x - y)).Sum();
+Console.WriteLine(diffSum);
+
+// Part2
+int similarity = leftColumn.Select(l => l * rightColumn.Count(r => r == l)).Sum();
+Console.WriteLine(similarity);
