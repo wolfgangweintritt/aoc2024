5e65b35bef5eb08727d0c26d835a866b4f542796
Wolfgang Weintritt <wolfgang.weintritt@gmail.com>
2024-12-26 17:05:06 +0100

    Add day3 part 2

--- day3/Solver.cs
+++ day3/Solver.cs
@@ -7,7 +7,55 @@ public class Solver
         string filePath = "input.txt";
         string text = File.ReadAllText(filePath).Trim();
 
-        // text = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        // text = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        
+        var products = Part1(text);
+        Console.WriteLine(products.Sum());
+        
+        // part 2
+        products = Part2(text);
+        Console.WriteLine(products.Sum());
+    }
+
+    private static IEnumerable<int> Part2(string text)
+    {
+        var matches = Regex.Matches(text, @"mul\(\d+,\d+\)|don't\(\)|do\(\)");
+        
+        foreach (Match m in matches)
+        {
+            Console.WriteLine(m.Value);
+        }
+        
+        bool multiply = true;
+        
+        IEnumerable<int> products = matches
+            .Where(m =>
+            {
+                if (m.Value == "do()")
+                {
+                    multiply = true;
+                    return false;
+                };
+
+                if (m.Value == "don't()")
+                {
+                    multiply = false;
+                    return false;
+                }
+
+                return multiply;
+            })
+            .Select(m =>
+            {
+                int commaIndex = m.Value.IndexOf(',');
+                return int.Parse(m.Value.Substring(4, commaIndex - 4)) * int.Parse(m.Value.Substring(commaIndex + 1, m.Value.Length - commaIndex - 2));
+            });
+        
+        return products;
+    }
+
+    private static IEnumerable<int> Part1(string text)
+    {
         var matches = Regex.Matches(text, @"mul\(\d+,\d+\)");
 
         foreach (Match m in matches)
@@ -21,7 +69,6 @@ public class Solver
                 int commaIndex = m.Value.IndexOf(',');
                 return int.Parse(m.Value.Substring(4, commaIndex - 4)) * int.Parse(m.Value.Substring(commaIndex + 1, m.Value.Length - commaIndex - 2));
             });
-        
-        Console.WriteLine(products.Sum());
+        return products;
     }
 }
