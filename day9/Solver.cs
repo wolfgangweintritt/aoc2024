6f7685cb6dcb9b4255ce6065908dc2740d6b8a30
Wolfgang Weintritt <wolfgang.weintritt@gmail.com>
2024-12-26 16:43:01 +0100

    Add day9 part 2

--- day9/Solver.cs
+++ day9/Solver.cs
@@ -9,28 +9,33 @@ public class Solver
 
         // text = "2333133121414131402";
 
+        var compactDisk = DefragPart1(text);
+        // Console.WriteLine(String.Join(',', compactDisk));
+
+        long checkSum = CalculateCheckSum(compactDisk);
+        Console.WriteLine(checkSum);
+
+    	// part2
+        var compactDisk2 = DefragPart2(text);
+        // Console.WriteLine(String.Join(',', compactDisk2));
+        
+        checkSum = CalculateCheckSum(compactDisk2);
+        Console.WriteLine(checkSum);
+    }
+
+    private static IList<int> DefragPart1(string text)
+    {
         List<int> disk = new List<int>();
         for (int i = 0; i < text.Length; i++)
         {
             int size = int.Parse(text[i].ToString());
-            if (i % 2 == 0)
-            {
-                disk.AddRange(new int[size].Select(x => i / 2));
-            }
-            else
-            {
-                disk.AddRange(new int[size].Select(x => -1));
-            }
+            disk.AddRange(new int[size].Select(x => i % 2 == 0 ? i / 2 : -1));
         }
-        
-        Console.WriteLine(String.Join(',', disk));
 
         List<int> reverseDiskWithoutSpaces = Enumerable
             .Reverse(disk)
             .Where(x => x != -1)
             .ToList();
-        
-        Console.WriteLine(String.Join(',', reverseDiskWithoutSpaces));
 
         int reverseIndex = 0;
         IList<int> compactDisk = disk
@@ -50,17 +55,72 @@ public class Solver
                     return reverseDiskWithoutSpaces[reverseIndex++];
                 })
             .ToList();
+        return compactDisk;
+    }
 
-        Console.WriteLine(String.Join(',', compactDisk));
+    private static IList<int> DefragPart2(string text)
+    {
+        LinkedList<(int, int)> linkedListDisk = new LinkedList<(int, int)>(text
+            .Select((f, i) => (f - '0', i % 2 == 0 ? i / 2 : -1))
+        );
+    
+        var fileToInsert = linkedListDisk.Last;
+        while (fileToInsert != null)
+        {
+            if (fileToInsert.Value.Item2 == -1) // skip free space
+            {
+                fileToInsert = fileToInsert.Previous;
+                continue;
+            }
+            
+            var file = linkedListDisk.First;
+            var nextFileToInsert = fileToInsert.Previous;
+            while (file != null)
+            {
+                if (fileToInsert == file)
+                {
+                    break;
+                }
+                
+                if (file.Value.Item2 != -1 || file.Value.Item1 < fileToInsert.Value.Item1) // skip files and 2 smol free space
+                {
+                    file = file.Next;
+                    continue;
+                }
+    
+                if (nextFileToInsert != null) // increase free space at move point
+                {
+                    nextFileToInsert.Value = nextFileToInsert.Value with { Item1 = nextFileToInsert.Value.Item1 + fileToInsert.Value.Item1 };
+                }
+                linkedListDisk.Remove(fileToInsert);
+                linkedListDisk.AddBefore(file, fileToInsert);
+                
+                // remove free space at insert point
+                file.Value = file.Value with { Item1 = file.Value.Item1 - fileToInsert.Value.Item1 };
+                
+                break;
+            }
+            fileToInsert = nextFileToInsert;
+        }
 
-        long checkSum = compactDisk
+        
+        List<int> compactDisk = new List<int>();
+        foreach (var file in linkedListDisk)
+        {
+            compactDisk.AddRange(new int[file.Item1].Select(x => file.Item2 == -1 ? -1 : file.Item2));
+        }
+        
+        return compactDisk;
+    }
+
+    private static long CalculateCheckSum(IList<int> compactDisk)
+    {
+        return compactDisk
             .Select((x, i) => new { Value = x, Index = i })
             .Aggregate(
                 0L,
                 (acc, x) => acc + (x.Value == -1 ? 0 : x.Value) * x.Index
             );
-        
-        Console.WriteLine(checkSum);
     }
 }
 
